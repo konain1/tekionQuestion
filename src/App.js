66dea5a3a7314b432b34b8b12cb4eb88895a4685
Konain1 <kaunainkasab@gmail.com>
2023-11-27 17:58:56 +0530

    fetch users

--- src/App.js
+++ src/App.js
@@ -1,8 +1,42 @@
 
-
+import axios from 'axios';
+import {useEffect,useState} from 'react'
 function App() {
+
+
+  const [userData,setUserData] = useState()
+
+
+  useEffect(()=>{
+
+
+  const fetchUsers = async()=>{
+    const res =  await axios.get('https://jsonplaceholder.typicode.com/users')
+    const Data = res.data
+    setUserData(Data)
+  }
+
+
+    fetchUsers();
+    console.log(userData)
+
+  },[])
+
+  
   return (
     <div className="App">
+
+    {userData?.map((item,key)=>{
+      return(
+        <>
+          <h1>{item.username}</h1>
+          <p>
+            {item.phone}
+          </p>
+          <p>{item.company.name}</p>
+        </>
+      )
+    })}
      
     </div>
   );
